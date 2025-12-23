c9edcce951d6dca9baabb2db161787e3562492a8
ReggeMeister <mattiaswaern04@gmail.com>
2025-12-23 23:36:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //DOM
-
 const searchInput = document.getElementById(search-input);
+const searchButton = document.getElementById(search-button);
+const typeFilter = document.getElementById(type-filter);
+const resetButton = document.getElementById(reset-btn);
+const prevButton = document.getElementById(prev-btn);
+const nextButton = document.getElementById(next-btn);
+const pokemonGrid = document.getElementById(pokemon-grid);
+const pageInfo = document.getElementById(page-info);
