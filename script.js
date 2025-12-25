c27b94361e6fd38c3d6c90411b38b18b22124255
ReggeMeister <mattiaswaern04@gmail.com>
2025-12-25 19:41:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-//DOM
-const searchInput = document.getElementById(search-input);
-const searchButton = document.getElementById(search-button);
-const typeFilter = document.getElementById(type-filter);
-const resetButton = document.getElementById(reset-btn);
-const prevButton = document.getElementById(prev-btn);
-const nextButton = document.getElementById(next-btn);
-const pokemonGrid = document.getElementById(pokemon-grid);
-const pageInfo = document.getElementById(page-info);
-const siteError = document.getElementById(error);
+// DOM Elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const resultsInfo = document.getElementById('results-info');
+const typeFilter = document.getElementById('type-filter');
+const resetButton = document.getElementById('reset-btn');
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+const pokemonGrid = document.getElementById('pokemon-grid');
+const pageInfo = document.getElementById('page-info');
+const error = document.getElementById('error');
+const errorText = document.getElementById('error-text');
+const loading = document.getElementById('loading');
+
+// Card Elements
+const card = document.getElementById('card');
+const closeCard = document.getElementById('close-card');
+const cardName = document.getElementById('card-name');
+const cardId = document.getElementById('card-id');
+const cardImage = document.getElementById('card-image');
+const cardTypes = document.getElementById('card-types');
+const cardHeight = document.getElementById('card-weight');
+const cardWeight = document.getElementById('card-weight');
+const cardExp = document.getElementById('card-exp');
+const cardAbilities = document.getElementById('card-abilities');
+const cardStats = document.getElementById('card-stats');
+const cardMoves = document.getElementById('card-moves');
+const cardEvolution = document.getElementById('card-evolution');
